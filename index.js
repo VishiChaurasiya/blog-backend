8fdf53def2b0b6ca3a05731665504276cdbba36a
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-05-29 05:22:26 +0530

    fix cors error

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const postRoute = require("./routes/post");
 const tagRoute = require("./routes/tag");
+const cors = require("cors");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-console.log(process.env.MONGODB);
+const app = express();
 
 connectToMongoDB(process.env.MONGODB)
   .then(() => console.log("Mongodb connected"))
   .catch((err) => console.log({ err }));
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/blog/post", postRoute);
